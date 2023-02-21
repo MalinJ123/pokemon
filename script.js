89667be8a2c1d531d82bd682e0fa27058423eefb
MalinJ123 <malin--jonsson@hotmail.com>
2023-02-21 17:31:55 +0100

    select btn i cards

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ choosePokemonBtn.addEventListener('click', async () => {
    const pokemonData = await getPokemonData()
    const popOverlayWhiteChoosePokemon = createChoosePokemonOverlay()
    renderPokemonData(pokemonData.results, popOverlayWhiteChoosePokemon)
+          
+  //  createCardSelectBTn(renderPokemonData) ; 
 })
 
 function createMyTeamPopOverlay() {
@@ -52,6 +54,23 @@ async function getPokemonData() {
    let popOverlayChoosePokemon = document.createElement("div")
    let popOverlayWhiteChoosePokemon = document.createElement("div")
    let chooseHeader = document.createElement("h2")
+var inputElement = document.createElement("input");
+inputElement.classList.add('input')
+inputElement.setAttribute("type", "text");
+inputElement.setAttribute("name", "myInput");
+inputElement.placeholder = 'Search pokemon'
+
+var selectBtn = document.createElement("button");
+selectBtn.textContent = "Select";
+selectBtn.classList.add("selectBtn");
+
+var inputContainer = document.createElement("div");
+inputContainer.classList.add("input-container");
+
+inputContainer.appendChild(inputElement);
+
+popOverlayWhiteChoosePokemon.appendChild(inputContainer);
+
    chooseHeader.innerText = "Välj pokemon"
    chooseHeader.classList.add("choose-header")
    popOverlayChoosePokemon.classList.add("popOverlay-choosePokemon")
@@ -72,23 +91,40 @@ async function getPokemonData() {
 
    return popOverlayWhiteChoosePokemon
  }
-
  function renderPokemonData(pokemonData, popOverlayWhiteChoose) {
-   if (pokemonData) {
-     pokemonData.forEach(pokemon => {
-       let cardDiv = document.createElement("div");
-       let cardImg = document.createElement("img");
-       let name = document.createElement("h4");
-       cardDiv.classList.add("myChooseteam-card");
-       cardImg.classList.add("myChooseteam-image");
-       name.classList.add("pokemonNametag");
-       cardImg.src = pokemon.sprites?.front_default;
-       name.innerText = pokemon.name;
-       
-       popOverlayWhiteChoose.append(cardDiv);
-       cardDiv.append(cardImg);
-       cardDiv.append(name);
-     });
-   }
- }
- 
+  if (pokemonData) {
+    pokemonData.forEach(pokemon => {
+      let cardDiv = document.createElement("div");
+      let cardImg = document.createElement("img");
+      let name = document.createElement("h4");
+      let selectBtn = document.createElement("button");
+
+      cardDiv.classList.add("myChooseteam-card");
+      cardImg.classList.add("myChooseteam-image");
+      name.classList.add("pokemonNametag");
+      selectBtn.classList.add("selectBtn");
+      selectBtn.textContent = "Select";
+
+      cardImg.src = pokemon.sprites?.front_default;
+      name.innerText = pokemon.name;
+      
+      popOverlayWhiteChoose.append(cardDiv);
+      cardDiv.append(cardImg);
+      cardDiv.append(name);
+      cardDiv.append(selectBtn);
+      
+      selectBtn.addEventListener('click', () => {
+          console.log('Select button clicked');
+          // add your select button logic here
+
+      });
+    });
+  }
+}
+
+function createCardSelectBTn(cardDiv) {
+  let selectBtn = document.createElement('button');
+  selectBtn.classList.add('selectBtn');
+  selectBtn.textContent = 'Select';
+  cardDiv.append(selectBtn);
+}
