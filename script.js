58a2b5b210d2b725cecb18a04ebc391d16355d08
MalinJ123 <malin--jonsson@hotmail.com>
2023-02-22 12:21:07 +0100

    david hjälpte mig med vad som känndes som miljoner buggar

--- script.js
+++ script.js
@@ -3,95 +3,127 @@ let choosePokemonBtn = document.querySelector('#choosePokemon-btn')
 let body = document.querySelector('body')
 
 myTeamBtn.addEventListener('click', () => {
-   createMyTeamPopOverlay()
+  createMyTeamPopOverlay()
 })
 // --------------------
 choosePokemonBtn.addEventListener('click', async () => {
+  // Hämta all information
   const pokemonData = await getPokemonData()
-  const popOverlayWhiteChoosePokemon = createChoosePokemonOverlay()
-  renderPokemonData(pokemonData.results, popOverlayWhiteChoosePokemon)
+  console.log('Hämtade data från API. En pokemon är: ', pokemonData.results[0], pokemonData.results[0].sprites)
+
 })
 // -------------------
 
 function createMyTeamPopOverlay() {
-   let popOverlayMyTeamBtn = document.createElement("div")
-   let PopOverlayWhite = document.createElement("div")
-   let myTeamHeader = document.createElement("h2")
-   myTeamHeader.classList.add("myTeamHeader")
-   myTeamHeader.innerText = 'Mitt Team'
-   popOverlayMyTeamBtn.classList.add("PopOverlay-MyTeamBtn")
-   PopOverlayWhite.classList.add("popOverlayWhite")
-
-   body.append(popOverlayMyTeamBtn)
-   popOverlayMyTeamBtn.append(PopOverlayWhite)
-   PopOverlayWhite.append(myTeamHeader)
-
-   PopOverlayWhite.addEventListener('click', (event) => {
-      console.log('du klickade på den vita overlayen')
-      event.stopPropagation()
-   })
-   popOverlayMyTeamBtn.addEventListener('click', () => {
-      console.log('du klickade på den svarta overlayen')
-      popOverlayMyTeamBtn.remove()
-   })
+  let popOverlayMyTeamBtn = document.createElement("div")
+  let PopOverlayWhite = document.createElement("div")
+  let myTeamHeader = document.createElement("h2")
+  myTeamHeader.classList.add("myTeamHeader")
+  myTeamHeader.innerText = 'Mitt Team'
+  popOverlayMyTeamBtn.classList.add("PopOverlay-MyTeamBtn")
+  PopOverlayWhite.classList.add("popOverlayWhite")
+  
+  body.append(popOverlayMyTeamBtn)
+  popOverlayMyTeamBtn.append(PopOverlayWhite)
+  PopOverlayWhite.append(myTeamHeader)
+  
+  PopOverlayWhite.addEventListener('click', (event) => {
+    console.log('du klickade på den vita overlayen')
+    event.stopPropagation()
+  })
+  popOverlayMyTeamBtn.addEventListener('click', () => {
+    console.log('du klickade på den svarta overlayen')
+    popOverlayMyTeamBtn.remove()
+  })
 }
 
 
 
 
 async function getPokemonData() {
-   const url = 'https://pokeapi.co/api/v2/pokemon/'
- 
-   console.log('Nu hämtar Jag data i från API')
-   const response = await fetch(url)
-   const data = await response.json()
-   console.log('Nu VÄNTAR Jag på data i från API')
-   
-   console.log(' Nu borde jag ha fått datan och den ser ut såhär  : ' + JSON.stringify(data, null, 2))
+  // sätt limit i url till 1000, så hämtar du fler pokemon. Offset ex = 5, då byter den ut 5 (plockar ut 5 pokemon efter 5.e plats )
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+  
+  console.log('Nu hämtar Jag data i från API')
+  const response = await fetch(url)
+  const data = await response.json()
+  //  datan är ett objekt med results 
+  console.log('Nu VÄNTAR Jag på data i från API')
+  
+  console.log(' Nu borde jag ha fått datan och den ser ut såhär  :' , data )
+  
+  
+  // Problem: forEach väntar inte tills alla funktioner är färdiga
+  let numberComplete = 0
+  data.results.forEach( async pokemon => { 
+    
+    // console.log(' Här är vi inne i foreach och tittar på varje pokemon', pokemon)
+    
+    const pokemonUrl = pokemon.url 
+    const pokemonResponse = await fetch(pokemonUrl)
+    let responseData = await pokemonResponse.json()
+    // console.log('Här är resultatet på responsedata', responseData.sprites)
+    
+    pokemon.sprites = responseData.sprites.front_shiny
+    // pokemon.sprites är en bildlänk
+    numberComplete++
+    if( numberComplete === data.results.length ) {
+      // nu har vi hämtat alla bildlänkar
+      // dags att rendera
+      const popOverlayWhiteChoosePokemon = createChoosePokemonOverlay();
+		  // renderPokemonData(pokemonData.results, popOverlayWhiteChoosePokemon)
 
-   return data
- }
- function createChoosePokemonOverlay() {
-   let popOverlayChoosePokemon = document.createElement("div")
-   let popOverlayWhiteChoosePokemon = document.createElement("div")
-   let chooseHeader = document.createElement("h2")
-let inputElement = document.createElement("input");
-inputElement.classList.add('input')
-inputElement.setAttribute("type", "text");
-inputElement.setAttribute("name", "myInput");
-inputElement.placeholder = 'Search pokemon'
-
-let selectBtn = document.createElement("button");
-selectBtn.textContent = "Select";
-selectBtn.classList.add("selectBtn");
-
-let inputContainer = document.createElement("div");
-inputContainer.classList.add("input-container");
-
-inputContainer.appendChild(inputElement);
-
-popOverlayWhiteChoosePokemon.appendChild(inputContainer);
-
-   chooseHeader.innerText = "Välj pokemon"
-   chooseHeader.classList.add("choose-header")
-   popOverlayChoosePokemon.classList.add("popOverlay-choosePokemon")
-   popOverlayWhiteChoosePokemon.classList.add("popOverlayWhite-choosePokemon")
-
-   body.append(popOverlayChoosePokemon)
-   popOverlayChoosePokemon.append(popOverlayWhiteChoosePokemon)
-   popOverlayWhiteChoosePokemon.append(chooseHeader)
-
-   popOverlayWhiteChoosePokemon.addEventListener("click", (event) => {
-     console.log("du klickade Select knappen")
-     event.stopPropagation()
-   })
-   popOverlayChoosePokemon.addEventListener("click", () => {
-     console.log("du klickade på den svarta overlayen")
-     popOverlayChoosePokemon.remove()
-   })
-
-   return popOverlayWhiteChoosePokemon
- }
+      // Skicka arrayen med pokemon-objekt
+      // pokemonCardDiv(pokemonData.results, popOverlayWhiteChoosePokemon);
+      renderFilteredPokemonData(data.results, popOverlayWhiteChoosePokemon);
+    }
+  });
+  
+  return data
+}
+// Returnerar det vita elementet som är i mitten av overlayen
+function createChoosePokemonOverlay() {
+  let popOverlayChoosePokemon = document.createElement("div")
+  let popOverlayWhiteChoosePokemon = document.createElement("div")
+  let chooseHeader = document.createElement("h2")
+  let inputElement = document.createElement("input");
+  
+  inputElement.classList.add('input')
+  inputElement.setAttribute("type", "text");
+  inputElement.setAttribute("name", "myInput");
+  inputElement.placeholder = 'Search pokemon'
+  
+  let selectBtn = document.createElement("button");
+  selectBtn.textContent = "Select";
+  selectBtn.classList.add("selectBtn");
+  
+  let inputContainer = document.createElement("div");
+  inputContainer.classList.add("input-container");
+  
+  inputContainer.appendChild(inputElement);
+  
+  popOverlayWhiteChoosePokemon.appendChild(inputContainer);
+  
+  chooseHeader.innerText = "Välj pokemon"
+  chooseHeader.classList.add("choose-header")
+  popOverlayChoosePokemon.classList.add("popOverlay-choosePokemon")
+  popOverlayWhiteChoosePokemon.classList.add("popOverlayWhite-choosePokemon")
+  
+  body.append(popOverlayChoosePokemon)
+  popOverlayChoosePokemon.append(popOverlayWhiteChoosePokemon)
+  popOverlayWhiteChoosePokemon.append(chooseHeader)
+  
+  popOverlayWhiteChoosePokemon.addEventListener("click", (event) => {
+    console.log("du klickade Select knappen")
+    event.stopPropagation()
+  })
+  popOverlayChoosePokemon.addEventListener("click", () => {
+    console.log("du klickade på den svarta overlayen")
+    popOverlayChoosePokemon.remove()
+  })
+  
+  return popOverlayWhiteChoosePokemon
+}
 function createCardSelectBTn(cardDiv) {
   let selectBtn = document.createElement('button');
   selectBtn.classList.add('selectBtn');
@@ -102,7 +134,7 @@ function createCardSelectBTn(cardDiv) {
 function renderPokemonData(pokemonData, popOverlayWhiteChoose) {
   if (pokemonData) {
     pokemonData.forEach(pokemon => {
-      // ----------------------------- The search btn in choosePokemon Overlayen
+      
       const inputElement = popOverlayWhiteChoose.querySelector('.input');
       inputElement.addEventListener('input', () => {
         const inputValue = inputElement.value.toLowerCase();
@@ -110,29 +142,34 @@ function renderPokemonData(pokemonData, popOverlayWhiteChoose) {
         renderFilteredPokemonData(filteredPokemonData, popOverlayWhiteChoose);
       });
       
-
-// -----------------------------Visar pokemon lista
+      // -----------------------------Visar pokemon lista
       let cardDiv = document.createElement("div");
       let cardImg = document.createElement("img");
       let name = document.createElement("h4");
       let selectBtn = document.createElement("button");
-
+      
       cardDiv.classList.add("myChooseteam-card");
       cardImg.classList.add("myChooseteam-image");
       name.classList.add("pokemonNametag");
       selectBtn.classList.add("selectBtn");
       selectBtn.textContent = "Select";
-
-      cardImg.src = pokemon.sprites?.front_default || '';
-      name.innerText = pokemon.name;
+      
+      cardImg.src = pokemon.sprites
+      // pokemon.sprites är en bildlänk
+      
+      
+      name.innerText = pokemon.name
+      pokemon.sprites = responseData.sprites.front_shiny
+      
+      console.log('Här är någoting med bilden ', pokemon.sprites )
       
       popOverlayWhiteChoose.append(cardDiv);
       cardDiv.append(cardImg);
       cardDiv.append(name);
       cardDiv.append(selectBtn);
-
+      
       selectBtn.addEventListener('click', () => {
-          console.log('Du klickade på select knappen ');
+        console.log('Du klickade på select knappen ');
       });
     });
   }
@@ -140,47 +177,58 @@ function renderPokemonData(pokemonData, popOverlayWhiteChoose) {
 function renderFilteredPokemonData(pokemonData, popOverlayWhiteChoose) {
   // Clear the existing cards
   popOverlayWhiteChoose.querySelectorAll('.myChooseteam-card').forEach(card => card.remove());
-
-  // Render the filtered results
+  
   if (pokemonData) {
-    pokemonData.forEach(pokemon => {
-      // ----------------------------- The search btn in choosePokemon Overlayen
-      const inputElement = popOverlayWhiteChoose.querySelector('.input');
-      inputElement.addEventListener('input', () => {
-        const inputValue = inputElement.value.toLowerCase();
-        const filteredPokemonData = pokemonData.filter(pokemon => pokemon.name.includes(inputValue));
-        renderFilteredPokemonData(filteredPokemonData, popOverlayWhiteChoose);
-      });
-      
-
-// -----------------------------Visar NYA filltrerade pokemon lista
-      let cardDiv = document.createElement("div");
-      let cardImg = document.createElement("img");
-      let name = document.createElement("h4");
-      let selectBtn = document.createElement("button");
-
-      cardDiv.classList.add("myChooseteam-card");
-      cardImg.classList.add("myChooseteam-image");
-      name.classList.add("pokemonNametag");
-      selectBtn.classList.add("selectBtn");
-      selectBtn.textContent = "Select";
-
-      cardImg.src = pokemon.sprites?.front_default || '';
-      name.innerText = pokemon.name;
-      
-      popOverlayWhiteChoose.append(cardDiv);
-      cardDiv.append(cardImg);
-      cardDiv.append(name);
-      cardDiv.append(selectBtn);
-
-      selectBtn.addEventListener('click', () => {
-          console.log('Du klickade på select knappen ');
-      });
-    });
-  }
+    pokemonCardDiv(pokemonData, popOverlayWhiteChoose);
+  };
+  
 }
 
+function pokemonCardDiv(pokemonData, popOverlayWhiteChoose) {
+  // Detta görs en gång
+  console.log('pokemonCardDiv: pokemonData är:', pokemonData)
+  const inputElement = popOverlayWhiteChoose.querySelector(".input");
+  //  console.log('Finns det någon input? ', inputElement)
+  inputElement.addEventListener("input", () => {
+    const inputValue = inputElement.value.toLowerCase();
+    const filteredPokemonData = pokemonData.filter((pokemon) =>
+      pokemon.name.includes(inputValue)
+    );
+    renderFilteredPokemonData(filteredPokemonData, popOverlayWhiteChoose);
+  });
 
+  pokemonData.forEach((pokemon) => {
+    // Detta görs flera gånger (1 per pokemon)
+		// ----------------------------- The search btn in choosePokemon Overlayen
+
+
+		let cardDiv = document.createElement("div");
+		let cardImg = document.createElement("img");
+		let name = document.createElement("h4");
+		let selectBtn = document.createElement("button");
+
+		cardDiv.classList.add("myChooseteam-card");
+		cardImg.classList.add("myChooseteam-image");
+		name.classList.add("pokemonNametag");
+		selectBtn.classList.add("selectBtn");
+		selectBtn.textContent = "Select";
+
+		// cardImg.src = pokemon.sprites || '';
+		name.innerText = pokemon.name;
+		cardImg.src = pokemon.sprites;
+    console.log('Kontrollera vad en pokemon är: ', pokemon)
+		console.log("Kontrollera renderfunktionen:", name.innerText, cardImg.src);
+
+		popOverlayWhiteChoose.append(cardDiv);
+		cardDiv.append(cardImg);
+		cardDiv.append(name);
+		cardDiv.append(selectBtn);
+
+		selectBtn.addEventListener("click", () => {
+			console.log("Du klickade på select knappen ");
+		});
+	});
+}  //pokemonCardDiv
 
 
 
