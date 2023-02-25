fea6395558dc3b9b670b5219ce0587c004fe5021
MalinJ123 <malin--jonsson@hotmail.com>
2023-02-25 23:20:50 +0100

    abilitys kommer tillbaka som undefined

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ choosePokemonBtn.addEventListener("click", async () => {
 	console.log(
 		"Hämtade data från API. En pokemon är: ",
 		pokemonData.results[0],
-		pokemonData.results[0].sprites
+		pokemonData.results[0].sprites,
+		pokemonData.results[0].abilities
 	);
 });
 
@@ -45,7 +46,9 @@ function createMyTeamPopOverlay() {
 	myTeam.forEach((pokemon, index) => {
 	  const newPokemonCard = document.createElement("div");
 	  const newPokemonImg = document.createElement("img");
-	  const newPokemonName = document.createElement("h6");
+	  const newPokemonName = document.createElement("h4");
+	  const newAbility = document.createElement("h6");
+
   
 	  const newChangeNameBtn = document.createElement("button");
 	  const newNameInput = document.createElement("input");
@@ -53,6 +56,8 @@ function createMyTeamPopOverlay() {
 	  const newRemoveBtn = document.createElement("button");
 	  newPokemonCard.classList.add("pokemon");
 	  newRemoveBtn.classList.add("removeBtn");
+	  newAbility.classList.add('ability')
+
   
 	  newChangeNameBtn.innerText = "Rename";
 	  newNameInput.classList.add("nameInput");
@@ -72,9 +77,13 @@ function createMyTeamPopOverlay() {
 		newDownBtn.innerText = "Down";
 		newDownBtn.classList.add("downBtn");
 
-	  newPokemonCard.append(newPokemonName, newPokemonImg, newChangeNameBtn, newNameInput);
-	  newPokemonImg.src = pokemon.sprites;
-	  newPokemonName.innerText = pokemon.name;
+	  newPokemonCard.append(newPokemonName, newPokemonImg,  newAbility, newChangeNameBtn, newNameInput,);
+
+			 //   lägg till abilitys här
+			 newPokemonImg.src = pokemon.sprites;
+			 newPokemonName.innerText = pokemon.name;
+			 newAbility.innerText = pokemon.abilities;
+
   
 	  newChangeNameBtn.addEventListener("click", (event) => {
 		event.stopPropagation();
@@ -146,7 +155,7 @@ function createMyTeamPopOverlay() {
 
 // ---------------
 
-  
+//   inte mer än 3 i myteam
 function addPokemonToTeam(pokemon) {
 	console.log("Test");
   
@@ -186,14 +195,16 @@ async function getPokemonData() {
 	// Problem: forEach väntar inte tills alla funktioner är färdiga
 	let numberComplete = 0;
 	data.results.forEach(async (pokemon) => {
-		// console.log(' Här är vi inne i foreach och tittar på varje pokemon', pokemon)
 
 		const pokemonUrl = pokemon.url;
 		const pokemonResponse = await fetch(pokemonUrl);
 		let responseData = await pokemonResponse.json();
-		// console.log('Här är resultatet på responsedata', responseData.sprites)
+	
 
-		pokemon.sprites = responseData.sprites.front_shiny;
+		// Om du vill ha shiny byt på två ställen i koden 
+		// pokemon.sprites = responseData.sprites.front_shiny;
+		pokemon.sprites = responseData.sprites.front_default;
+		pokemon.abilities = responseData.abilities.ability;
 		// pokemon.sprites är en bildlänk
 		numberComplete++;
 		if (numberComplete === data.results.length) {
@@ -285,25 +296,30 @@ function renderPokemonData(pokemonData, popOverlayWhiteChoose) {
 			let cardDiv = document.createElement("div");
 			let cardImg = document.createElement("img");
 			let name = document.createElement("h4");
+			let ability = document.createElement("h6");
+
 			let selectBtn = document.createElement("button");
 
 			cardDiv.classList.add("myChooseteam-card");
 			cardImg.classList.add("myChooseteam-image");
 			name.classList.add("pokemonNametag");
+			ability.classList.add("ability");
 			selectBtn.classList.add("selectBtn");
 			selectBtn.textContent = "Select";
 
 			cardImg.src = pokemon.sprites;
-			// pokemon.sprites är en bildlänk
+			ability = responseData.abilities;
 
 			name.innerText = pokemon.name;
-			pokemon.sprites = responseData.sprites.front_shiny;
+			// pokemon.sprites = responseData.sprites.front_shiny;
+			pokemon.sprites = responseData.sprites.front_default;
 
-			console.log("Här är någoting med bilden ", pokemon.sprites);
+			// console.log("Här är någoting med bilden ", pokemon.sprites);
 
 			popOverlayWhiteChoose.append(cardDiv);
-			cardDiv.append(cardImg);
 			cardDiv.append(name);
+			cardDiv.append(cardImg);
+			cardDiv.append(ability);
 			cardDiv.append(selectBtn);
 
 		
@@ -319,17 +335,16 @@ function renderFilteredPokemonData(pokemonData, popOverlayWhiteChoose) {
 
 	if (pokemonData) {
 		pokemonCardDiv(pokemonData, popOverlayWhiteChoose);
-		// createCardSelectBTn(cardDiv);
 	} else if (pokemonData) {
 		pokemonData.forEach((pokemon) => {
-			// ...
+			
 			cardDiv.addEventListener("click", () => {
 				console.log("du klickade på en pokemon");
 				selectedPokemon = pokemon;
 			});
 
 			createCardSelectBtn(cardDiv);
-			// ...
+			
 		});
 	}
 }
@@ -354,14 +369,21 @@ function pokemonCardDiv(pokemonData, popOverlayWhiteChoose) {
 		let cardDiv = document.createElement("div");
 		let cardImg = document.createElement("img");
 		let name = document.createElement("h5");
+		let ability = document.createElement("h6");
+
 		let selectBtn = document.createElement("button");
 
 		cardDiv.classList.add("myChooseteam-card");
 		cardImg.classList.add("myChooseteam-image");
 		name.classList.add("pokemonNametag");
 		selectBtn.classList.add("selectBtn");
+		ability.classList.add("ability");
 		selectBtn.textContent = "Select";
 
+		
+		
+		// console.log('detta är vad pokemon ability är ', pokemon.ability)
+		ability.innerText = pokemon.abilities
 		name.innerText = pokemon.name;
 		cardImg.src = pokemon.sprites;
 		// console.log('Kontrollera vad en pokemon är: ', pokemon)
@@ -370,7 +392,12 @@ function pokemonCardDiv(pokemonData, popOverlayWhiteChoose) {
 		popOverlayWhiteChoose.append(cardDiv);
 		cardDiv.append(name);
 		cardDiv.append(cardImg);
+		cardDiv.append(ability);
 		cardDiv.append(selectBtn);
+
+
+	
+// --------------------------------------------------------------------------------------------------
 
     // Den här Click funktionen fungerar. 
 		selectBtn.addEventListener("click", () => {
@@ -386,8 +413,3 @@ function pokemonCardDiv(pokemonData, popOverlayWhiteChoose) {
 
 
 
-// const toggleTipText = document.querySelector('span')
-		// 	toggleTipText.classList.add('toggleTipText')
-		// 	toggleTipText.innerText = 'Du kan inte ha mer än tre pokemon i ditt team'
-		// 	const overlayContent = createChoosePokemonOverlay.querySelector('.overlay-content');
-		// 	overlayContent.appendChild(toggleTipText);
